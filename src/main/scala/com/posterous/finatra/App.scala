8f812fc17bca34ae3a15c711478927b3a709fa9a
Julio Capote <jcapote@gmail.com>
2012-02-17 11:38:29 -0800

    hello world wihtou the jboss shit

--- src/main/scala/com/posterous/finatra/App.scala
+++ src/main/scala/com/posterous/finatra/App.scala
@@ -1,40 +1,37 @@
 package com.posterous.finatra
 
-import com.twitter.finagle.{Service, SimpleFilter}
-import org.jboss.netty.handler.codec.http._
-import org.jboss.netty.handler.codec.http.HttpResponseStatus._
-import org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1
-import org.jboss.netty.buffer.ChannelBuffers.copiedBuffer
-import org.jboss.netty.util.CharsetUtil.UTF_8
-import com.twitter.util.Future
 import java.net.InetSocketAddress
+import java.util.{NoSuchElementException => NoSuchElement}
+import org.jboss.netty.handler.codec.http.HttpMethod._
+import org.jboss.netty.buffer.ChannelBuffers.copiedBuffer
+import com.twitter.util.Future
+import org.jboss.netty.util.CharsetUtil.UTF_8
+import com.twitter.finagle.http.{Http, RichHttp, Request, Response}
+import com.twitter.finagle.http.Status._
+import com.twitter.finagle.http.Version.Http11
+import com.twitter.finagle.http.path._
+import com.twitter.finagle.{Service, SimpleFilter}
 import com.twitter.finagle.builder.{Server, ServerBuilder}
-import com.twitter.finagle.http.Http
+
+
 /**
  * @author ${user.name}
  */
 object App {
 
-  class HelloWorld extends Service[HttpRequest, HttpResponse]{  
+  class HelloWorld extends Service[Request, Response]{  
 
     var routes: Map[String, Function0[Int]] = Map()
 
-    def get(path: String, callback:() => Int) { 
-      //val result = callback()
-      //println(result)
-      routes += path -> callback 
-      //val num = routes.get("/").getOrElse(null)()
-      //println(num)
-    } 
-
-    get("/", () => 
-      2 + 2
-    )
-
-    def apply(request: HttpRequest) = {
-      val response = new DefaultHttpResponse(HTTP_1_1, OK)
-      response.setContent(copiedBuffer("hello world", UTF_8))
+    def apply(request: Request) = {
+      val response = Response(Http11, InternalServerError)
+      response.mediaType = "text/plain" 
+      response.content = copiedBuffer("asd", UTF_8)
       Future.value(response)
+      //#Responses.json("a", acceptsGzip(request))
+      //val response = new DefaultHttpResponse(HTTP_1_1, OK)
+      //response.setContent(copiedBuffer("hello world", UTF_8))
+      //kkFuture.value(response)
     }
   }
 
@@ -43,7 +40,7 @@ object App {
     val helloworld = new HelloWorld
 
     val server: Server = ServerBuilder()
-      .codec(Http())
+      .codec(RichHttp[Request](Http()))
       .bindTo(new InetSocketAddress(7070))
       .name("helloworld")
       .build(helloworld)
