643bd6cd467c2a464f5bab6ac09b89c9546a2ca3
Julio Capote <julio@twitter.com>
2012-07-09 18:47:32 -0700

    trait

---
+++ src/main/scala/com/posterous/finatra/FinatraOldController.scala
@@ -0,0 +1,5 @@
+package com.posterous.finatra
+
+import com.capotej.finatra_core.{AbstractFinatraController,FinatraRequest}
+
+//class FinatraController extends AbstractFinatraController[Request, FinatraResponse]
